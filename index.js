eb9f86e1f5d529e5969d798bfcbcd9f2b6652694
CharlesKibet <charlesbiegon973@gmail.com>
2024-03-30 14:12:38 +0300

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'bob';
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+  console.log(customerName);
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+  console.log(bestCustomer);
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'charlie'
+}
